009a9f571874a6693ded1b958d8d5ad5c2112a13
Binoyog <sowrovahmed6117@gmail.com>
2026-01-04 02:02:27 +0600

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+<!-- js/firebase.js -->
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiKuc8l-6RTZWf5FwUMEbUuMiMUUQ3lYM",
+  authDomain: "joypurhat-express-delivery.firebaseapp.com",
+  projectId: "joypurhat-express-delivery",
+  storageBucket: "joypurhat-express-delivery.appspot.com",
+  messagingSenderId: "423594341774",
+  appId: "1:423594341774:web:fd33f5a2d368e79e3e20df"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+</script>
